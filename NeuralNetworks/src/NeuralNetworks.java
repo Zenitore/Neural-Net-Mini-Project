84eeda862d8494ebf5ad11b7c20d966351c56b21
Zenitore <eayers25@kentdenver.org>
2024-04-22 22:00:20 -0600

    Shifted the focus from completing the neural network to instead just beginning the foundation of it. To finish it I would require 200 different hidden layers, and 15000 different training examples sooooo... anyways added in the beginnings of the neural network foundation. This is our last day to work on this so even though there hasnt been much coding completed a lot of my time went to research on the topic.

--- NeuralNetworks/src/NeuralNetworks.java
+++ NeuralNetworks/src/NeuralNetworks.java
@@ -1,22 +1,41 @@
+import javax.rmi.CORBA.Util;
+import java.util.Random;
+
 public class NeuralNetworks {
     private double density;
     private double velocity;
     private double wingArea;
     private double liftCoef;
     private double liftForce;
+    private double bias;
+    private double weight1;
+    private double weight2;
+    private Random random;
 
 
     public NeuralNetworks(double density, double velocity){
-        liftCoef = 0.4;
+        random = new Random();
+        bias = random.nextDouble();
+        weight1 = random.nextDouble();
+        weight2 = random.nextDouble();
         wingArea = 174;
         this.velocity = velocity;
         this.density = density;
     }
 
-    public double calculate(){
-        liftForce = liftCoef * (density*velocity*velocity)/2.0 * wingArea;
+    public class Util {
+        public static double sigmoid(double in){
+            return 1 / (1 + Math.exp(-in));
+        }
+    }
+
+    public double calculate(double input1, double input2){
+        double preActivation = (weight1*input1) + (weight2*input2) + bias;
+        double output = Util.sigmoid(preActivation);
         return liftForce;
     }
+
+
 
     public double getWingArea() {
         return wingArea;
